78a735c40d9b42dc67766452cdea13aa10f1a38d
juanf1328 <juanf1328@gmail.com>
2021-11-18 17:52:39 -0300

    clear commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, {Component} from 'react';
-import Navigation from './components/Navigation/Navigation';
+import React from 'react';
+import Navigation from './Navigation/Navigation';
+//import Logo from './Logo/logo';
 import './App.css';
 
-class App extends Component{
+class App extends React.Component{
   render(){
     return(
       <div className="App">
         <Navigation />
+        
         
       </div>
       );
