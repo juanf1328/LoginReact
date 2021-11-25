ba1cb504c29d0d174302e32b0a9e41cfa922e97e
juanf1328 <juanf1328@gmail.com>
2021-11-25 17:22:04 -0300

    render null commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React, { useState } from 'react';
 import Login from './components/Login';
 
+function App() {
+  const adminUser = {
+    email: "admin@gmail.com",
+    password: "admin75"
+  }
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
 
-/*function App(){
-  
-  const admin = {
-    email:"admin@gmail.com",
-    password:"administrador"
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password){
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      console.log("Details do not match!");
+      setError("Details do not match!");
+    }
   }
 
-
-const [user, setUser] = useState({name: "", email: ""});
-const [error, setError] = useState("");
-
-const Login = details => {
-  console.log(details);
-}
-
-const Logout = () => {
-  console.log("Logout");
-}
-
+  const Logout = () => {
+    setUser ({ name: "", email: ""});
+  }
 
   return (
-     
-          <Login />
-      
-
-      
+    <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+        ) : (
+        <Login Login={Login} error={error} />
+        )}
+    </div>
     );
-}*/ 
+}
 
-const App = () => <Login />
+
   
 export default App;
